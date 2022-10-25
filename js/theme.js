c8e2980c8b8fa83862e559d2c543cb3d11c52292
jiwidi <fhjaime96@gmail.com>
2022-10-25 18:13:40 +0200

    attempt to fix flashing

--- js/theme.js
+++ js/theme.js
@@ -47,3 +47,17 @@ function toggle(a) {
     }
     set_theme(a)
 };
+
+// Helper function
+let domReady = (cb) => {
+    document.readyState === 'interactive' || document.readyState ===
+        'complete' ?
+        cb() :
+        document.addEventListener('DOMContentLoaded', cb);
+};
+
+domReady(() => {
+    // Display body when DOM is loaded
+    document.body.style.visibility = 'visible';
+    // document.body.style.opacity = '1';
+});
