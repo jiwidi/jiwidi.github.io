ee882d2c5596e839b7c5f78c0e650e39ff1c2fb2
jiwidi <fhjaime96@gmail.com>
2024-02-14 18:52:44 +0200

    add analytics and insights

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,17 @@
       <Navbar />
       <router-view />
     </div>
+
+    <SpeedInsights />
+
   </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
-// import { init } from '@vercel/analytics';
+import { inject } from '@vercel/analytics';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+inject();
+
 
 export default {
     name: 'App',
