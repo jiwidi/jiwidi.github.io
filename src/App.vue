f022ed4c3374427417bb0883cce55d2e3a023b57
jiwidi <fhjaime96@gmail.com>
2024-03-06 15:43:40 +0100

    seo content

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,24 @@ export default {
     name: 'App',
     components: {
         Navbar
-    }
+    },
+    metaInfo: {
+        title: 'Jaime Ferrando Huertas - Personal Page',
+        meta: [
+            {
+                name: 'description',
+                content: 'Personal portfolio of Your Name showcasing projects, skills, and professional experience in web development.' // Tailor this description to match your professional focus
+            },
+            {
+                name: 'keywords',
+                content: 'personal portfolio, Your Name, web development, projects, professional experience, skills' // Customize these keywords to match your skills and focus areas
+            },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { name: 'robots', content: 'index, follow' },
+            { name: 'author', content: 'https://github.com/jiwidi' },
+            { rel: 'canonical', href: 'https://www.imjai.me/' },
+        ]
+    },
 };
 </script>
 
