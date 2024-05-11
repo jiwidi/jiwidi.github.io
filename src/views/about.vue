d85bace7f4ed60ce3402412f67e9c2d556e2b1a4
Andres <anerdspual@outlook.com>
2024-05-11 11:26:55 +0200

    fixed issues 1,2 & 3

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,45 +1,50 @@
 <template>
-    <div class="about-container">
-      <main class="shadowPlus4">
-        <article>
-            <h1>About me</h1>
-            <div style="width: 50%; float: left;"> I was born in Spain and
-					have been tinkering with computers since I was 8 years old.
-					A few quotes I resonate with:
-					<p> - Be kind.</p>
-					<p> - Being normal is overrated.</p>
-					<p> - I care about what people make, not what they own.</p>
-				</div>
-				<div style="margin-left: 50%; text-align:right; "> <img
-						src="/src/assets/img/about/author_feroe.jpeg" width="50%"> </div>
-                    </article>
+  <div class="about-container">
+    <main class="shadowPlus4">
+      <article class="about-article">
+        <div>
+          <h1>About me</h1>
+          <div>
+            I was born in Spain and have been tinkering with Computers since I was 8 years old.
+            A few quotes I resonate with:
+            <p>- Be kind.</p>
+            <p>- Being normal is overrated.</p>
+            <p>- I care about what people make, not what they own.</p>
+          </div>
+        </div>
+        
+        <div class="about-image">
+          <img src="/src/assets/img/about/author_feroe.jpeg" />
+        </div>
+      </article>
       <listDisplay :listItems="pages" />
-      </main>
-    </div>
-  </template>
+    </main>
+  </div>
+</template>
 
 <script>
 import listDisplay from '/src/components/listDisplay.vue';
 
 export default {
-    name: 'About',
-    components: {
-        listDisplay
+  name: 'About',
+  components: {
+    listDisplay,
+  },
+  methods: {
+    toggleTheme() {
     },
-    methods: {
-        toggleTheme() {
-        }
-    },
-    data() {
-        return {
-            pages: [
-                {
-                    title: "Curriculum Vitae",
-                    link: "https://github.com/jiwidi/CurriculumVitae/blob/master/JaimeFerrandoHuertas_CV.pdf", // Make sure this link is handled by your Vue router
-                    icon: "newspaper_icon",
-                },
-            ],
-        };
-    },
+  },
+  data() {
+    return {
+      pages: [
+        {
+          title: 'Curriculum Vitae',
+          link: 'https://github.com/jiwidi/CurriculumVitae/blob/master/JaimeFerrandoHuertas_CV.pdf',
+          icon: 'newspaper_icon',
+        },
+      ],
+    };
+  },
 };
 </script>
+
