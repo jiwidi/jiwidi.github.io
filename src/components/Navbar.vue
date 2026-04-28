a2773b64bbbe406411a2bcf13e4f711094036283
jiwidi <fhjaime96@gmail.com>
2026-04-28 16:10:22 +0200

    improv web

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,48 +1,65 @@
 <template>
-    <div class="topnav">
-      <span>
-        <router-link to="/">//jaime</router-link>
-        <span v-for="(segment, index) in pathSegments" :key="index">
-          <span v-if="index > 0"></span>
-          <router-link :to="getPathUpToSegment(index)">
-            /{{ segment }}
-          </router-link>
-        </span>:
-        <span class="blink_me">_</span>
-      <ThemeToggle />
-      </span>
-      <ThemeToggle />
-    </div>
-  </template>
+  <nav class="topnav" aria-label="path">
+    <span class="crumbs">
+      <router-link to="/">//jaime</router-link>
+      <template v-for="(segment, index) in pathSegments" :key="index">
+        <span class="sep">/</span>
+        <router-link :to="getPathUpToSegment(index)">{{ segment }}</router-link>
+      </template>
+      <span>:</span>
+      <span class="blink_me">_</span>
+    </span>
+
+    <span class="right">
+      <router-link
+        v-if="pathSegments.length > 0"
+        :to="parentPath"
+        class="back-btn"
+      >← back</router-link>
+      <button class="theme-btn" type="button" @click="toggleTheme">
+        {{ themeLabel }}
+      </button>
+    </span>
+  </nav>
+</template>
 
 <script>
-import ThemeToggle from './themeToggle.vue';
-
 export default {
-    components: {
-        ThemeToggle
+  name: 'Navbar',
+  data() {
+    return {
+      theme: 'light',
+    };
+  },
+  computed: {
+    pathSegments() {
+      return this.$route.path.split('/').filter(Boolean);
     },
-    computed: {
-        pathSegments() {
-            // Split the current path into segments
-            return this.$route.path.split('/').filter(Boolean);
-        },
+    parentPath() {
+      if (this.pathSegments.length <= 1) return '/';
+      return '/' + this.pathSegments.slice(0, -1).join('/');
     },
-    methods: {
-        getPathUpToSegment(index) {
-            // Generate the path up to the clicked segment
-            return '/' + this.pathSegments.slice(0, index + 1).join('/');
-        },
+    themeLabel() {
+      return this.theme === 'dark' ? '☼ light' : '☾ dark';
     },
+  },
+  methods: {
+    getPathUpToSegment(index) {
+      return '/' + this.pathSegments.slice(0, index + 1).join('/');
+    },
+    applyTheme(t) {
+      this.theme = t;
+      document.documentElement.setAttribute('data-theme', t);
+      try { localStorage.setItem('theme', t); } catch (e) { /* noop */ }
+    },
+    toggleTheme() {
+      this.applyTheme(this.theme === 'dark' ? 'light' : 'dark');
+    },
+  },
+  mounted() {
+    let saved = 'light';
+    try { saved = localStorage.getItem('theme') || 'light'; } catch (e) { /* noop */ }
+    this.applyTheme(saved);
+  },
 };
 </script>
-
-<style scoped>
-.topnav {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-
-/* Rest of the styles */
-</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,15 +1,13 @@
 <template>
-    <div class="home-container">
-      <main class="shadowPlus4">
-        <article>
-          <h1>Hello there!</h1>
-          <p>I'm <a href="about/" class="abold">{Jaime}</a> a
-            <strike>data scientist, engineer, geek</strike> who likes to build stuff at the computer. Apart from coding my interests include photography, snow sports and cycling.</p>
-          <p>I'm currently building <a href="https://ecomid.com" class="abold">eComID</a> as co-founder and head of AI.</p>
-        </article>
-        <listDisplay :listItems="pages" />
-      </main>
-    </div>
+    <main>
+      <article>
+        <h1>Hello there!</h1>
+        <p>I'm <router-link to="/about" class="abold">{Jaime}</router-link> a
+          <strike>data scientist, engineer, geek</strike> who likes to build stuff at the computer. Apart from coding my interests include photography, snow sports and cycling.</p>
+        <p>I'm currently building <a href="https://ecomid.com" target="_blank" rel="noopener" class="abold">eComID</a> as co-founder and head of AI.</p>
+      </article>
+      <listDisplay :listItems="pages" :numbered="true" />
+    </main>
   </template>
 
 <script>
@@ -23,36 +21,12 @@ export default {
     data() {
         return {
             pages: [
-                {
-                    title: "Projects",
-                    link: "/projects",
-                    icon: "folder_icon",
-                },
-                {
-                    title: "Writing",
-                    link: "/writing",
-                    icon: "folder_icon",
-                },
-                {
-                    title: "Creative",
-                    link: "/creative",
-                    icon: "folder_icon",
-                },
-                {
-                    title: "Keyboards",
-                    link: "/keyboards",
-                    icon: "folder_icon",
-                },
-                {
-                    title: "Photography",
-                    link: "/photography",
-                    icon: "folder_icon",
-                },
-                {
-                    title: "About",
-                    link: "/about",
-                    icon: "folder_icon",
-                }
+                { title: "Projects",    link: "/projects",    description: "12 repos" },
+                { title: "Writing",     link: "/writing",     description: "11 pieces" },
+                { title: "Creative",    link: "/creative",    description: "9 finds" },
+                { title: "Keyboards",   link: "/keyboards",   description: "7 builds" },
+                { title: "Photography", link: "/photography", description: "8 sets" },
+                { title: "About",       link: "/about",       description: "readme" },
             ],
         };
     },
