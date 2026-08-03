b086cd68b496a7698c2d598bc1aadd94a5f0b85c
jiwidi <fhjaime96@gmail.com>
2026-08-03 13:21:09 +0200

    refine photography introduction

--- src/views/photography.vue
+++ src/views/photography.vue
@@ -4,11 +4,11 @@
       <main class="shadowPlus4">
         <article>
           <h1>The art of noticing</h1>
-            <p>I believe great photographs capture moments of life that awaken a memory or a feeling.</p>
+            <p>I believe life holds more beauty than we usually see. How much of it we experience depends, in part, on our ability to pay attention.</p>
 
-        <p>These moments are everywhere. Most are brief and unplanned. Some happen every day, while others may happen only once. Together, they remember us something about life and who we are.</p>
+        <p>These moments are everywhere. Most are brief and unplanned. Some happen every day, while others happen only once. Together, they remind us of something about life and who we are.</p>
 
-        <p>Noticing these moments and finding a way to preserve them is the part of photography I love most. The photograph keeps the moment, but the real art is noticing it in the first place.</p>
+        <p>Noticing these moments and finding a way to preserve them is the part of photography I love most. A photograph may keep the moment, but the real art is noticing it in the first place.</p>
 <photoGrid/>
         </article>
         <BackButton />
